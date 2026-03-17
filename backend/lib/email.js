0cff4c60782a5c2bf1f6e8823ab48c98e643777c
surajarya1999 <surajarya460@gmail.com>
2026-03-17 11:15:35 +0530

    new

--- backend/lib/email.js
+++ backend/lib/email.js
@@ -6,7 +6,7 @@ console.log("EMAIL_FROM:", process.env.EMAIL_FROM);
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
